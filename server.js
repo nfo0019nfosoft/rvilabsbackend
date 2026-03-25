f9a727e5fe7a26a83dd73ed4096c7e8500de1502
nfo0019nfosoft <nfo0019nfosoft@gmail.com>
2026-03-25 15:53:23 +0530

    rvibackend

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // 🔥 ADD THIS
 
